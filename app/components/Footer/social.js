fe2e3564d391b5ea483874fc5011929d9e302bbe
babychh <63610192+babychh@users.noreply.github.com>
2020-04-14 17:04:10 +0300

    Update social.js

--- app/components/Footer/social.js
+++ app/components/Footer/social.js
@@ -9,11 +9,11 @@ function Social() {
     return (
     <div id="wrapper">
           <div className="social_icons">
-            <a href="#"><img src={facebook} alt="facebook icon"/></a>
-            <a href="#"><img src={instagram} alt="instagram icon"/></a>
-            <a href="#"><img src={twitter} alt="twitter icon"/></a>
+            <a href="https://m.facebook.com/altas.cars.3?ref=m_nux_wizard"><img src={facebook} alt="facebook icon"/></a>
+            <a href="https://www.instagram.com/altascars/"><img src={instagram} alt="instagram icon"/></a>
+            <a href="https://t.me/altascars"><img src={twitter} alt="twitter icon"/></a>
           </div>
-          <p>autoservice@gmail.com</p>
+          <p>altascars1@gmail.com</p>
       </div>
     );
   }
