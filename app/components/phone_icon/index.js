17a985b51a71a0a779745e48b987d265a028354c
babychh <63610192+babychh@users.noreply.github.com>
2020-04-17 22:50:22 +0300

    Update index.js

--- app/components/phone_icon/index.js
+++ app/components/phone_icon/index.js
@@ -8,7 +8,7 @@ export default function MyPhone() {
   return (
 
     <div>
-    <a class="phone_call" href="tel:+380953160534">
+    <a class="phone_call" href="tel:+380637389193">
 
         <img src={phone} alt="phone"/>
         
